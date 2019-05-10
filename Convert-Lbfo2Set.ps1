745ebb88ee26091877b32f6b33a52e15ce259f00
James Kehr <40303902+JamesKehr@users.noreply.github.com>
2019-05-09 22:35:50 -0400

    Template for Convert-Lbfo2Set
    Creating the basic template for the script. Not adding any context yet.
    
    Need to investigate what modules are needed per OS version. Hyper-V seems to have all the WS2019 commands, but NetSwitchTeam could be the WS2016 module...?

---
+++ Convert-Lbfo2Set.ps1
@@ -0,0 +1,60 @@
+# Main conversion script.
+
+#requires -Version 5.1
+#requires -RunAsAdministrator
+#requires -Modules NetLbfo, NetSwitchTeam, Hyper-V
+
+<#
+HELP SECTION
+
+#>
+
+<#
+To-do
+
+ - Everything...
+
+#>
+
+######################
+###   PARAMETERS   ###
+######################
+
+param (
+
+)
+
+
+######################
+###   VARIABLES    ###
+######################
+#region
+
+#endregion
+
+
+######################
+###   FUNCTIONS    ###
+######################
+#region
+
+#endregion
+
+
+######################
+###   VALIDATION   ###
+######################
+#region
+
+#endregion
+
+
+######################
+###      MAIN      ###
+######################
+
+
+
+
+
+Write-Verbose "Work complete!"
