b51daf036c5c643c4b041dc34d2dab3963c577bb
Dan Cuomo <dcuomo@users.noreply.github.com>
2020-01-10 15:24:00 -0800

    Adding example

---
+++ helpers/Convert Host vNIC Example.ps1
@@ -0,0 +1,18 @@
+﻿Remove-VMNetworkAdapter -ManagementOS -SwitchName LBFOSwitch
+Remove-VMNetworkAdapter -ManagementOS -SwitchName SETSwitch
+
+1..10 | Foreach-Object {
+	Add-VMNetworkAdapter -ManagementOS -Name "LBFOHVNIC0$_" -SwitchName LBFOSwitch
+}
+
+cls
+
+Get-VMNetworkAdapter -ManagementOS | ?{$_.SwitchName -eq 'LBFOSwitch' -or $_.SwitchName -eq 'SETSwitch'}
+
+Measure-Command {
+	1..10 | Foreach-Object {
+		C:\temp\LBFOMigration\nvspinfo.exe -r "LBFOHVNIC0$_" SETSwitch
+	}
+}
+
+Get-VMNetworkAdapter -ManagementOS | ?{$_.SwitchName -eq 'LBFOSwitch' -or $_.SwitchName -eq 'SETSwitch'}
